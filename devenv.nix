c2a9e381c0d58e4c27afb6b08586cda43c8aef25
Mike Georgeff <mike@georgeff.co>
2026-07-02 13:17:07 -0500

    add container build

--- devenv.nix
+++ devenv.nix
@@ -10,6 +10,23 @@ in {
     php.packages.composer
   ];
 
+  containers.cli = {
+    name = "cli";
+    entrypoint = [ "php" "/app/bin/junction" ];
+    copyToRoot = pkgs.buildEnv {
+      name = "cli-app";
+      paths = [
+        (pkgs.runCommand "app" { } ''
+          mkdir -p $out/app
+          cp -r ${./src} $out/app/src
+          cp -r ${./bin} $out/app/bin
+          cp -r ${./migrations} $out/app/migrations
+          cp -r ${./vendor} $out/app/vendor
+        '')
+      ];
+    };
+  };
+
   enterShell = ''
     set +x
     set -a; [ -f .env ] && source .env; set +a
